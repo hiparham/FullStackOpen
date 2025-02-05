8d8b2c4a38ebeb8399790d39b95cc4e437f8c2da
hiparham <parhamdevv@gmail.com>
2025-02-05 16:14:59 +0330

    Part4 DONE

--- Part4/tests/AddBlogToken.test.js
+++ Part4/tests/AddBlogToken.test.js
@@ -7,3 +7,25 @@ const User = require("../models/User");
 const ConnectDb = require("../utils/ConnectDB");
 const app = require("../app");
 const api = supertest(app);
+beforeEach(async () => {
+  await ConnectDb();
+});
+//
+test("Proper Token Post", async () => {
+  await api
+    .post("/api/blogs")
+    .send({
+      content: "Testing From Vscode",
+      url: "Supertest",
+      author: "Supertest",
+    })
+    .set({
+      authorization:
+        "Bearer ShittyToken",
+    })
+    .expect(401)
+});
+//
+after(async () => {
+  await mongoose.connection.close();
+});
