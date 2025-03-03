b87cf0fa9756fb44c51d87803102b690fb5c69fd
hiparham <parhamdevv@gmail.com>
2025-03-03 16:43:13 +0330

    Exercises for React

---
+++ Part9/FlightDiaries/Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-stone-900 min-h-screen">
+      <h1 className="text-center text-white py-[2vh] text-[1.5rem] font-roboto font-[600] capitalize tracking-[.5px]">
+        Flight diaries app
+      </h1>
+    </div>
+  );
+}
